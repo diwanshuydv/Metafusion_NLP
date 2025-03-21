f652117140ab262202fc94daa3fe4a3d0bc061d8
diwanshuydv <b23cs1017@iitj.ac.in>
2025-03-21 21:41:42 +0000

    Added: verl PPO training script

---
+++ ppo/run.sh
@@ -0,0 +1,25 @@
+python3 -m verl.trainer.main_ppo \
+ data.train_files= ppo/data_parquet/try_1.parquet \
+ data.val_files= ppo/data_parquet/e_try_1.parquet \
+ data.train_batch_size=256 \
+ data.max_prompt_length=512 \
+ data.max_response_length=256 \
+ actor_rollout_ref.model.path= models/pretrained_models \
+ actor_rollout_ref.actor.optim.lr=1e-6 \
+ actor_rollout_ref.actor.ppo_mini_batch_size=64 \
+ actor_rollout_ref.actor.ppo_micro_batch_size=1 \
+ actor_rollout_ref.rollout.log_prob_micro_batch_size_per_gpu=1 \
+ actor_rollout_ref.rollout.tensor_model_parallel_size=1 \
+ actor_rollout_ref.rollout.gpu_memory_utilization=0.4 \
+ actor_rollout_ref.ref.log_prob_micro_batch_size_per_gpu=4 \
+ critic.optim.lr=1e models/pretrained_models \
+ critic.ppo_micro_batch_size=1 \
+ algorithm.kl_ctrl.kl_coef=0.001 \
+ +trainer.val_before_train=False \
+ trainer.default_hdfs_dir=null \
+ trainer.n_gpus_per_node=1 \
+ trainer.nnodes=1 \
+ trainer.save_freq=10 \
+ trainer.test_freq=10 \
+ trainer.total_epochs=15 \
+ trainer.logger=\[console\]
