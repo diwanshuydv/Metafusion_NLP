ebef7e44d19b467baa8efeb985255d63d05b6094
diwanshuydv <b23cs1017@iitj.ac.in>
2025-05-19 13:17:02 +0530

    Fixed: Issue in train config loading

--- sft/config/training_config.py
+++ sft/config/training_config.py
@@ -1,24 +1,24 @@
 from unsloth import is_bfloat16_supported
-from config.model_config import fast_model_config
+from .model_config import fast_model_config
 
 training_argument = {
     "per_device_train_batch_size": 64,
     "gradient_accumulation_steps": 1,
     "warmup_steps": 5,
-    "num_train_epochs": 1,
+    "num_train_epochs": 2,
     "learning_rate": 3e-5,
     "fp16": not is_bfloat16_supported(),
     "bf16": is_bfloat16_supported(),
     "logging_steps": 1,
     # "optim": "adamw_8bit",
     "weight_decay": 0.01,
-    "lr_scheduler_type": "linear",
+    "lr_scheduler_type": "cosine_with_restarts",
     "seed": 3407,
     "output_dir": "/home/raid/metafusion/WARPxMetafusion/outputs",
     "report_to": "none",
     # "report_to": "tensorboard",
     "eval_strategy": "steps",
-    "eval_steps": 50,
+    "eval_steps": 10,
     # "max_steps": 100,
 }
 
